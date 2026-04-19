2b85c5d6181baa762d243789b4f13b31832391df
Neu-Electron <1833299046@qq.com>
2026-04-19 14:47:39 +0800

    feat: solve LeetCode 2946 matrix similarity after cyclic shifts
    - Use periodic check instead of actual array rotation
    - Compare mat[i][j] with mat[i][(j+k)%n] for all rows
    - Unify left/right shift logic into single condition
    - Time O(m*n), space O(1)

---
+++ leetcode/2946.matrix-similarity-after-cyclic-shifts.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    bool areSimilar(std::vector<std::vector<int>>& mat, int k) {
+        int m = mat.size();
+        int n = mat[0].size();
+        k %= n;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != mat[i][(j + k) % n]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
